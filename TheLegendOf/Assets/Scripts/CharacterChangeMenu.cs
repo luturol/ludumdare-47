ac95726e68744306712198fecd60073117057fc7
Rafael Ahrons <rafael.ahrons@gmail.com>
2020-10-04 13:23:07 -0300

    Add character change to image and logic to use when he is dead (#20)

---
+++ TheLegendOf/Assets/Scripts/CharacterChangeMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterChangeMenu : MonoBehaviour
+{
+    public CharacterDictionary[] characters;
+    [SerializeField] Image image;
+        
+    /// <summary>
+    /// Start is called on the frame when a script is enabled just before
+    /// any of the Update methods is called the first time.
+    /// </summary>
+    void Start()
+    {        
+        image.sprite = characters[(int) SceneLoader.currentCharacter].sprite;
+    }
+}
---
+++ TheLegendOf/Assets/Scripts/Characters.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public enum Characters
+{
+    Gladiator,
+    Archer,
+    Witcher
+}
+
+[Serializable]
+
+public class CharacterDictionary
+{
+    public Characters character;
+    public Sprite sprite;
+}
