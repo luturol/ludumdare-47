276e0fd8d5ab9e1c14f342820b41565d2e40bc92
Chokko <chokkolk@gmail.com>
2020-10-04 18:37:41 -0300

    Health bar added

---
+++ TheLegendOf/Assets/Scripts/Player/PlayerHealthBar.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    //Vida do Player
+    [SerializeField]public float playerHealth;
+
+    void Start()
+    {
+        playerHealth = 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            playerHealth--;
+        }
+        if (playerHealth <= 0)
+        {
+            playerHealth = 0;
+        }
+    }
+}
---
+++ TheLegendOf/Assets/Scripts/UI/HealthManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthManager : MonoBehaviour
+{
+
+    [SerializeField] Image hpImage;
+    [SerializeField] PlayerHealthBar playerHP;
+    [SerializeField] Sprite[] health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHP = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealthBar>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerHP.playerHealth == 0)
+        {
+            hpImage.sprite = health[0];
+        }
+        if (playerHP.playerHealth == 1)
+        {
+            hpImage.sprite = health[1];
+        }
+        if (playerHP.playerHealth == 2)
+        {
+            hpImage.sprite = health[2];
+        }
+    }
+}
